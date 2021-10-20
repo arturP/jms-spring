27654305a7130c59183be87018f484a5466aa029
Artur Przychodzen <artur2p@gmail.com>
2021-10-20 19:00:51 +0200

    initial

---
+++ src/main/java/io/artur/spring/jmsspring/JmsSpringApplication.java
@@ -0,0 +1,13 @@
+package io.artur.spring.jmsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/artur/spring/jmsspring/JmsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.jmsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
